78e965ae3ab99743b097500f3d485020b447fc11
Anas Riahi <anas.riahi@smile-maroc.com>
2019-08-06 10:17:23 +0100

    Initial Commit

---
+++ StoreSearch/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StoreSearch
+//
+//  Created by Anas Riahi on 8/6/19.
+//  Copyright © 2019 Anas Riahi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
