7a44ba87afdf7aff65183f0d60bbc62404ff789d
Anas Riahi <anas.riahi@smile-maroc.com>
2019-08-06 14:19:13 +0100

    Add tableview and searchView

---
+++ StoreSearch/SearchResult.swift
@@ -0,0 +1,15 @@
+//
+//  SearchResult.swift
+//  StoreSearch
+//
+//  Created by Anas Riahi on 8/6/19.
+//  Copyright © 2019 Anas Riahi. All rights reserved.
+//
+
+import Foundation
+
+class SearchResult {
+    var name = ""
+    var artistName = ""
+    
+}
